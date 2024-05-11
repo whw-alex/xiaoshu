a2fcf33b5370a3893084f544db6d966d7de65aba
王皓雯 <3178243510@qq.com>
2024-05-11 18:11:52 +0800

    update

--- frontend/xiaoshu/app/build.gradle.kts
+++ frontend/xiaoshu/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
